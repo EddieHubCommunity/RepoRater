1a6e4773ee8f3c75fd7b28c005a92bbaa06221bc
Eddie Jaoude <eddie@jaoudestudios.com>
2023-12-21 07:48:16 +0000

    fix: remove cache on repo list page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Repos from "@/components/Repos";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <main className="hero min-h-screen bg-base-200">

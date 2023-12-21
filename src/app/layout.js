1d0d7e894fb4fbdd75226c3aaa09f61248ee4ce8
Eddie Jaoude <eddie@jaoudestudios.com>
2023-12-21 09:36:52 +0000

    feat: star repo banner

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Alert from "@/components/Alert";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className="flex justify-center p-2 bg-green-500 text-black">
+          <Link
+            href="https://github.com/EddieHubCommunity/RepoRater"
+            target="_blank"
+          >
+            Support by giving the GitHub repo a STAR
+          </Link>
+        </div>
         <Navbar />
         {alert && <Alert message={message} />}
         {children}

d2437a49570f76322441b34b3e9aed470469d45a
Eddie Jaoude <eddie@jaoudestudios.com>
2023-12-25 18:56:59 +0000

    fix: reduce minimum popular votes to 3
    Increase as more repos get more votes

--- src/app/popular/page.js
+++ src/app/popular/page.js
@@ -3,7 +3,7 @@ import Repos from "@/components/Repos";
 export default function Popular() {
   return (
     <main className="hero min-h-screen bg-base-200">
-      <Repos sort={"votes"} minimumVotes={4} />
+      <Repos sort={"votes"} minimumVotes={3} />
     </main>
   );
 }

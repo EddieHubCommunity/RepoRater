d7d86615ad4bbabd6c75f3796a9f1432ad413298
Eddie Jaoude <eddie@jaoudestudios.com>
2023-12-26 08:04:10 +0000

    fix: remove cache

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Repos from "@/components/Repos";
 import Stats from "@/components/Stats";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-base-200 pt-6">

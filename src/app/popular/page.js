9acb1d8ec01acdc1882f8a4e9e5a06d0fb116ff9
Eddie Jaoude <eddie@jaoudestudios.com>
2023-12-26 09:19:51 +0000

    feat: stats added to popular page

--- src/app/popular/page.js
+++ src/app/popular/page.js
@@ -1,10 +1,12 @@
 import Repos from "@/components/Repos";
+import Stats from "@/components/Stats";
 
 export const dynamic = "force-dynamic";
 
 export default function Popular() {
   return (
-    <main className="hero min-h-screen bg-base-200">
+    <main className="min-h-screen bg-base-200 pt-6">
+      <Stats />
       <Repos sort={"votes"} minimumVotes={3} />
     </main>
   );

3b7a3e5e8d80884f91e204c8bc3c7089bdfe9b40
sohan paliyal <sohnpaliyal@gmail.com>
2023-12-30 12:26:09 +0530

    fixed navbar

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,14 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Repos from "@/components/Repos";
 import Stats from "@/components/Stats";
-
+import Navbar2 from '@/components/Navbar2'
 export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
+      <Navbar2/>
       <main className="min-h-screen bg-base-200 pt-6">
         <Stats />
         <Repos minimumVotes={1} />

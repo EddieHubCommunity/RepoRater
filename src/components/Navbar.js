f58c10b048e37ad7ae4a215ea9f76ad69feed166
sohan paliyal <sohnpaliyal@gmail.com>
2023-12-31 12:50:22 +0530

    navbar overflow fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,26 +13,12 @@ export default function Navbar() {
   const message = params.get("message");
   const [user, setUser] = useState(null);
 
-  const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);  
+  const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
 
   const logout = async () => {
     await account.deleteSession("current");
     setUser(null);
   };
-
-
-  const handleResize = (e) => {
-    if (e.currentTarget.innerWidth > 767) {
-        setIsMobileMenuVisible(false);
-    }
-}
-
-useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => {
-        window.removeEventListener('resize', handleResize);
-    };
-}, [handleResize]);
 
   const getUser = async () => {
     try {
@@ -68,41 +54,40 @@ useEffect(() => {
             </span>
           </p>
         </div>
-        <button className="md:hidden inline-block ms-auto" onClick={()=>setIsMobileMenuVisible(!isMobileMenuVisible)}>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6 -mb-1.5 ms-1"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16">
+        <button className="md:hidden inline-block ms-auto" onClick={() => setIsMobileMenuVisible(!isMobileMenuVisible)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 -mb-1.5 ms-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16">
 
-                            </path>
-                        </svg>
+            </path>
+          </svg>
         </button>
         <div className="flex-none">
-          <ul className={`menu menu-horizontal px-1 
-          ${!isMobileMenuVisible ? 'md:flex hidden'
-          :
-          'absolute top-12 left-0 right-0 bottom-0 h-[82dvh] flex flex-col bg-base-100 mobile-navbar'}`}>
+          <ul className={`menu menu-horizontal px-1 md:flex 
+          ${!isMobileMenuVisible ? 'hidden':
+              'absolute md:static md:flex-row md:h-[auto] top-12 left-0 right-0 bottom-0 h-[82dvh] flex flex-col bg-base-100 mobile-navbar'}`}>
             <li>
-              <Link href="/" onClick={()=>setIsMobileMenuVisible(false)}>List Ratings</Link>
+              <Link href="/" onClick={() => setIsMobileMenuVisible(false)}>List Ratings</Link>
             </li>
             <li>
-              <Link href="/popular" onClick={()=>setIsMobileMenuVisible(false)}>Popular Ratings</Link>
+              <Link href="/popular" onClick={() => setIsMobileMenuVisible(false)}>Popular Ratings</Link>
             </li>
             <li>
-              <Link href="/rankings" onClick={()=>setIsMobileMenuVisible(false)}>User Rankings</Link>
+              <Link href="/rankings" onClick={() => setIsMobileMenuVisible(false)}>User Rankings</Link>
             </li>
             {user && (
               <li>
-                <Link href="/rate" onClick={()=>setIsMobileMenuVisible(false)}>Add Rating</Link>
+                <Link href="/rate" onClick={() => setIsMobileMenuVisible(false)}>Add Rating</Link>
               </li>
             )}
             {!user && (
               <li>
-                <Link href="/auth/login" onClick={()=>setIsMobileMenuVisible(false)}>Login</Link>
+                <Link href="/auth/login" onClick={() => setIsMobileMenuVisible(false)}>Login</Link>
               </li>
             )}
             {user && (

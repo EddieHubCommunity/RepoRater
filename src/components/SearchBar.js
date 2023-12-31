3b4d992bd74b8ce921a4a4821fca3e4b3cf8fcb2
BuildTools <unconfigured@null.spigotmc.org>
2023-12-31 09:48:21 +0530

    Added SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,16 @@
-// components/SearchBar.js
 "use client";
-// components/SearchBar.js
 import { useState } from 'react';
-
+//the searchBar functionality should be added.
 const SearchBar = ({ data }) => {
   const [searchTerm, setSearchTerm] = useState('');
-  // Provide a default value (empty array) for filteredData
+  
   const [filteredData, setFilteredData] = useState(data || []);
 
   const handleSearch = (e) => {
     const term = e.target.value;
     setSearchTerm(term);
 
-    // Filter the data based on the search term
+   
     const filtered = data.filter(item =>
       item.toLowerCase().includes(term.toLowerCase())
     );
@@ -27,6 +25,7 @@ const SearchBar = ({ data }) => {
         placeholder="Search..."
         value={searchTerm}
         onChange={handleSearch}
+        className="bg-gray-300 text-white border-2 border-gray-100 rounded-md p-2 transition-all duration-500 hover:bg-gray-500 hover:border-black"
       />
       <ul>
         {filteredData.map((item, index) => (

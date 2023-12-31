41fba081fa6c0c7356eb4659369e6bb2241521f2
R Sai Deva Harsha <110902188+SaiDevaHarshaR@users.noreply.github.com>
2023-12-31 09:35:28 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,17 @@
-// components/SearchBar.js
+
 "use client";
-// components/SearchBar.js
+
 import { useState } from 'react';
 
 const SearchBar = ({ data }) => {
   const [searchTerm, setSearchTerm] = useState('');
-  // Provide a default value (empty array) for filteredData
+ 
   const [filteredData, setFilteredData] = useState(data || []);
 
   const handleSearch = (e) => {
     const term = e.target.value;
     setSearchTerm(term);
 
-    // Filter the data based on the search term
     const filtered = data.filter(item =>
       item.toLowerCase().includes(term.toLowerCase())
     );
@@ -36,5 +35,5 @@ const SearchBar = ({ data }) => {
     </div>
   );
 };
-
+//this searchBar functionality should be added
 export default SearchBar;

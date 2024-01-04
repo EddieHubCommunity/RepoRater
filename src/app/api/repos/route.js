fbab1502673d3241e90dfd2a7f8aac7bcfb3c54e
Eddie Jaoude <eddie@jaoudestudios.com>
2024-01-04 21:10:41 +0000

    feat: sort repos list

--- src/app/api/repos/route.js
+++ src/app/api/repos/route.js
@@ -8,6 +8,19 @@ export async function GET(request) {
   const params = request.nextUrl.searchParams;
   const minimumVotes = params.get("minimumVotes");
   const keyword = params.get("keyword");
+  const sort = params.get("sort");
+
+  let filter = [];
+  switch (sort) {
+    case "rated":
+      filter.push(Query.orderDesc("rating"));
+      break;
+    case "popular":
+      filter.push(Query.orderDesc("votes"));
+      break;
+    default:
+      filter.push(Query.orderDesc("rating"), Query.orderDesc("votes"));
+  }
 
   const repos = await new Databases(clientAdmin()).listDocuments(
     process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
@@ -23,8 +36,7 @@ export async function GET(request) {
         "name",
         "badgeViews",
       ]),
-      Query.orderDesc("rating"),
-      Query.orderDesc("votes"),
+      ...filter,
       minimumVotes && Query.greaterThanEqual("votes", parseInt(minimumVotes)),
       Query.limit(100),
       keyword && Query.search("url", keyword),
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 import SideNav from "@/components/SideNav";
 import Repos from "@/components/Repos";
@@ -8,90 +8,73 @@ import Activity from "@/components/Activity";
 import Stats from "@/components/Stats";
 import Toast from "@/components/Toast";
 import { useSearchParams } from "next/navigation";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronUpDownIcon } from "@heroicons/react/24/outline";
+import { classNames } from "@/utils/classNames";
 
 export const dynamic = "force-dynamic";
+
+const sortOptions = [
+  { name: "Highest rated", value: "rated" },
+  { name: "Most popular", value: "popular" },
+];
 
 export default function Page() {
   const params = useSearchParams();
   const alert = params.get("alert");
   const message = params.get("message");
   const [keyword, setKeyword] = useState("");
+  const [sort, setSort] = useState(sortOptions[0].value);
 
   return (
     <>
       <SideNav setKeyword={setKeyword}>
         <>
           <main className="lg:pr-96">
+            <Stats />
             <header className="flex items-center justify-between border-b border-white/5 px-4 py-4 sm:px-6 sm:py-6 lg:px-8">
-              <Stats />
-              {/* <h1 className="text-base font-semibold leading-7 text-white">
-              Repositories
-            </h1> */}
+              <h1 className="text-base font-semibold leading-7 text-white">
+                Repositories
+              </h1>
 
               {/* Sort dropdown */}
-              {/* <Menu as="div" className="relative">
-              <Menu.Button className="flex items-center gap-x-1 text-sm font-medium leading-6 text-white">
-                Sort by
-                <ChevronUpDownIcon
-                  className="h-5 w-5 text-gray-500"
-                  aria-hidden="true"
-                />
-              </Menu.Button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="absolute right-0 z-10 mt-2.5 w-40 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={classNames([
-                          active ? "bg-gray-50" : "",
-                          "block px-3 py-1 text-sm leading-6 text-gray-900",
-                        ])}
-                      >
-                        Name
-                      </a>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={classNames(
-                          active ? "bg-gray-50" : "",
-                          "block px-3 py-1 text-sm leading-6 text-gray-900"
-                        )}
-                      >
-                        Date updated
-                      </a>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={classNames([
-                          active ? "bg-gray-50" : "",
-                          "block px-3 py-1 text-sm leading-6 text-gray-900",
-                        ])}
-                      >
-                        Environment
-                      </a>
-                    )}
-                  </Menu.Item>
-                </Menu.Items>
-              </Transition>
-            </Menu> */}
+              <Menu as="div" className="relative">
+                <Menu.Button className="flex items-center gap-x-1 text-sm font-medium leading-6 text-white">
+                  Sort by
+                  <ChevronUpDownIcon
+                    className="h-5 w-5 text-gray-500"
+                    aria-hidden="true"
+                  />
+                </Menu.Button>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-10 mt-2.5 w-40 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                    {sortOptions.map((item) => (
+                      <Menu.Item>
+                        <span
+                          onClick={() => setSort(item.value)}
+                          className={classNames([
+                            sort === item.value ? "bg-gray-200" : "",
+                            "block px-3 py-1 text-sm leading-6 text-gray-900 cursor-pointer",
+                          ])}
+                        >
+                          {item.name}
+                        </span>
+                      </Menu.Item>
+                    ))}
+                  </Menu.Items>
+                </Transition>
+              </Menu>
             </header>
 
-            <Repos keyword={keyword} minimumVotes={0} />
+            <Repos keyword={keyword} minimumVotes={0} sort={sort} />
           </main>
 
           <aside className="bg-black/10 lg:fixed lg:bottom-0 lg:right-0 lg:top-16 lg:w-96 lg:overflow-y-auto lg:border-l lg:border-white/5">

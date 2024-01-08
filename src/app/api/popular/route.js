867df06143c7c0efecbd20e949f303ca9733a490
Eddie Jaoude <eddie@jaoudestudios.com>
2024-01-08 09:26:59 +0000

    chore: refactored popoular api

--- src/app/api/popular/route.js
+++ src/app/api/popular/route.js
@@ -8,36 +8,30 @@ export async function GET(request) {
   const params = request.nextUrl.searchParams;
   const minimumVotes = params.get("minimumVotes");
 
+  const fields = [
+    "url",
+    "logo",
+    "description",
+    "rating",
+    "votes",
+    "owner",
+    "name",
+    "badgeViews",
+  ];
   const repos = await new Databases(clientAdmin()).listDocuments(
     process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
     process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_REPOS_ID,
     [
-      Query.select([
-        "url",
-        "logo",
-        "description",
-        "rating",
-        "votes",
-        "owner",
-        "name",
-        "badgeViews",
-      ]),
+      Query.select(fields),
       Query.orderDesc("votes"),
       Query.greaterThanEqual("votes", parseInt(minimumVotes) || 5),
       Query.limit(100),
     ]
   );
 
-  const top = repos.documents.map((repo) => ({
-    url: repo.url,
-    logo: repo.logo,
-    description: repo.description,
-    rating: repo.rating,
-    votes: repo.votes,
-    owner: repo.owner,
-    name: repo.name,
-    badgeViews: repo.badgeViews,
-  }));
+  const data = repos.documents.map((repo) =>
+    Object.fromEntries(fields.map((field) => [[field], repo[field]]))
+  );
 
-  return Response.json(top);
+  return Response.json(data);
 }

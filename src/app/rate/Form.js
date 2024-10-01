732b8f22976f5629bf7c4149a1d3afed0b6072fe
Saini Madhu <madhusaini46810@gmail.com>
2024-10-01 16:39:00 +0530

    fix: auto focus to rating input (#126)

--- src/app/rate/Form.js
+++ src/app/rate/Form.js
@@ -83,6 +83,7 @@ export default function Form({ name = "", owner = "" }) {
                 value={owner && name && `https://github.com/${owner}/${name}`}
                 placeholder="URL"
                 className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                autoFocus
                 required
               />
             </div>
